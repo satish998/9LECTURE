01b4b34c21fb3939a4e935fee15a50268d3ae6a6
satish998 <100787234+satish998@users.noreply.github.com>
2023-07-20 22:52:51 +0530

    9lecture assignment

---
+++ function.js
@@ -0,0 +1,19 @@
+function area(){
+    let l=10;
+    let b=20;
+    console.log(l);
+    return l*b;
+}
+
+console.log(area());
+console.log("..................")
+var s=50;
+function demo(){
+    console.log(s)
+    let z=10;
+    function c(){
+        console.log("c function executed")
+    }
+    c();
+}
+demo()
